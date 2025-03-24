f79486a17fecc0019ab90d6a26a9f4c10f91d6d3
Felipe Moreira <felipe.moreiras796@gmail.com>
2025-03-24 21:04:16 +0100

    first commit

---
+++ src/extensions/loadIfcCommandSet/loc/myStrings.d.ts
@@ -0,0 +1,9 @@
+declare interface ILoadIfcCommandSetCommandSetStrings {
+  Command1: string;
+  Command2: string;
+}
+
+declare module 'LoadIfcCommandSetCommandSetStrings' {
+  const strings: ILoadIfcCommandSetCommandSetStrings;
+  export = strings;
+}
